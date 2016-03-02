bcfdce81c1586ff0cfb296b7e75c6fe101d9feff
taylormike <mike.j.taylor0@gmail.com>
2016-03-01 22:28:22 -0500

    Unit Tests Initial Checkin
    Unit Tests Initial Checkin

---
+++ ValueComparer.UnitTests/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueComparer.UnitTests
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ ValueComparer.UnitTests/VaueComparerTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using val = ValueComparison.ValueComparer;
+
+namespace ValueComparer.UnitTests
+{
+    [TestFixture]
+    public class VaueComparerTests
+    {
+        [TestCase]
+        public void ObjectComparison()
+        {
+            var cat1 = new Cat()
+            {
+                Name = "Cinder",
+                Type = "Tabby"
+            };
+
+            var cat2 = new Cat()
+            {
+                Name = "Cinder",
+                Type = "Tabby2"
+            };
+
+            var cat3 = new Cat()
+            {
+                Name = "Cinder",
+                Type = "Tabby"
+            };
+
+            Assert.False(val.AreEqual(cat1, cat2));
+            Assert.True(val.AreEqual(cat1, cat3));
+        }
+    }
+}
